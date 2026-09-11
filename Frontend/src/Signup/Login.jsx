b8aeb3a88640590c9095e66b8166ac8cdbb4e2fb
Piyush Dutta <pd75289@gmail.com>
2026-09-11 15:26:50 +0530

    Connect frontend with live backend

--- Frontend/src/Signup/Login.jsx
+++ Frontend/src/Signup/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/login",
+        "https://zerodha-clone-2n8x.onrender.com/login",
         {
           ...inputValue,
         },
--- Frontend/src/Signup/Signup.jsx
+++ Frontend/src/Signup/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/signup",
+        "https://zerodha-clone-2n8x.onrender.com/signup",
         {
           ...inputValue,
         },
